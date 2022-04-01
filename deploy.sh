b3a1f8a544f1d19fdd99f2c33057571ef2ad807d
mengjie xu <mengjiexu@MacBook-von-mengjie.local>
2022-04-01 10:08:24 +0200

    test

--- deploy.sh
+++ deploy.sh
@@ -37,10 +37,12 @@ echo ""
 echo "Committing changes to $(pwd)"
 git add .
 git commit -m "$msg"
+# git push https://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@github.com/xumj2021/myblog.dev.repo.git
+
 <<<<<<< HEAD
+>>>>>>> 451657b65a0442b21a3da12a3b5b429cc8cd4a90
 # git push origin master
-git push https://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@github.com/xumj2021/myblog.dev.repo.git
 =======
-# git push https://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@github.com/xumj2021/myblog.dev.repo.git
+git push https://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@github.com/xumj2021/myblog.dev.repo.git
 git push origin master
 >>>>>>> 451657b65a0442b21a3da12a3b5b429cc8cd4a90
